850c20ff907f3c180bdbffb2e6fa6c39b8e83f98
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2024-02-14 00:55:45 -0300

    Hash Map

---
+++ Colecoes.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Colecoes {
+  public static void main(String[] args) {
+    Map<String, Integer> notas = new HashMap<>(); // no hashMap se tiver chaves iguais o ultimo valor que é recuperado
+    notas.put("Dani", 10);
+    notas.put("Paulo", 9);
+    notas.put("Thiago", 5);
+    notas.put("Sergio", 10);
+
+    var nota = notas.get("Dani");
+    System.out.println(nota);
+
+    for (Map.Entry<String, Integer> entry : notas.entrySet()) {
+      String key = entry.getKey();
+      Integer value = entry.getValue();
+
+      System.out.println("A chave é " + key + " e o valor é " + value);
+
+    }
+
+  }
+}
