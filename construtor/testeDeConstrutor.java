ec5c854a0f0912b0c837c2beafd976c1404f09f7
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2024-02-27 01:57:54 -0300

    Construtores

---
+++ construtor/testeDeConstrutor.java
@@ -0,0 +1,8 @@
+package construtor;
+
+public class testeDeConstrutor {
+  public static void main(String[] args) {
+    Construtor construtor = new Construtor(1, "construtor");
+    Construtor construtor1 = new Construtor(2, null);
+  }
+}
