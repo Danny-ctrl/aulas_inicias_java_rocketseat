c13e11f9140895f5bdd4ed753b1c160f893d3e34
Daniele Inspiron <contatodanny@hotmail.com>
2024-10-20 20:00:21 -0300

    "for_break_continue"

---
+++ controle_de_fluxo/src/controle_de_fluxo/ExemploFlor.java
@@ -0,0 +1,13 @@
+package controle_de_fluxo;
+
+public class ExemploFlor {
+
+	public static void main(String[] args) {
+		int carneirinhos=1;
+for(;carneirinhos<=20;) {
+	System.out.println(carneirinhos + "- Carneirinho(s)");
+	carneirinhos ++;
+}
+	}
+
+}
---
+++ controle_de_fluxo/src/controle_de_fluxo/ForAlunos.java
@@ -0,0 +1,11 @@
+package controle_de_fluxo;
+
+public class ForAlunos {
+public static void main(String[] args) {
+	String alunos[] = {"FELIPE","JONAS","JULIA","MARCOS"};
+	
+	for(String aluno : alunos) {
+		System.out.println(aluno);
+	}
+}
+}
